44ab3fc392cfa793a6d7aa14533112a9f3af01cd
todd <todd@wireservers.com>
2025-03-07 19:21:01 -0600

    Module 1 - Creating and importing components

--- react-native/react-basics/src/App.jsx
+++ react-native/react-basics/src/App.jsx
@@ -2,10 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-
-function Heading() {
-  return <h1>Vite + React - This is an h1 heading</h1>;
-}
+import Heading from "./heading";
 
 function App() {
   const [count, setCount] = useState(0);
---
+++ react-native/react-basics/src/heading.jsx
@@ -0,0 +1,5 @@
+function Heading() {
+  return <h1>Vite + React - This is an h1 heading</h1>;
+}
+
+export default Heading;
