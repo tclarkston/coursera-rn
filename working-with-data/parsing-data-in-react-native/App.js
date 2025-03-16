a525ed16ae6a6a39a0fe150498acb6fb42c51ce0
todd <todd@wireservers.com>
2025-03-16 10:27:57 -0500

    Interacting with REST APIs in RN

---
+++ working-with-data/parsing-data-in-react-native/App.js
@@ -0,0 +1,54 @@
+import {FlatList, Text, SafeAreaView, View, StyleSheet } from 'react-native';
+import menuItems from './menuItems.json';
+
+const App = () => {
+  const { menu } = menuItems;
+
+  const Item = ({ name, price }) => (
+      <View style={menuStyles.innerContainer}>
+        <Text style={menuStyles.itemText}>{name}</Text>
+        <Text style={menuStyles.itemText}>{'$' + price}</Text>
+      </View>
+  );
+
+  const renderItem = ({ item }) => (
+      <Item name={item.title} price={item.price} />
+  );
+
+  return (
+      <SafeAreaView style={menuStyles.container}>
+        <Text style={menuStyles.headerText}>Little Lemon Menu</Text>
+        <FlatList
+            data={menu}
+            keyExtractor={({ id }) => id}
+            renderItem={renderItem}
+        />
+      </SafeAreaView>
+  );
+};
+
+export default App;
+
+const menuStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  innerContainer: {
+    paddingHorizontal: 40,
+    paddingVertical: 20,
+    backgroundColor: '#495E57',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  itemText: {
+    color: '#F4CE14',
+    fontSize: 22,
+  },
+  headerText: {
+    color: '#F4CE14',
+    fontSize: 30,
+    textAlign: 'center',
+    marginBottom: 20,
+    paddingTop: 40,
+  },
+});
